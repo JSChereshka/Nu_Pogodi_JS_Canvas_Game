065b684390b865528ca10e9bad0ddee85bbd5032
dogkami <k.primov92@gmail.com>
2016-10-11 01:59:35 +0300

    Made the logic for falling eggs. Still have to implement restarting the falling of the eggs and catching them from the wolf.

--- 00_Canvas_9.10.16.js
+++ 00_Canvas_9.10.16.js
@@ -1,22 +1,33 @@
 /**
  * Created by Ivan Minchev on 9.10.2016 г..
  */
+let wolfTopRight = {x: 155, y:50, w: 50, h: 60};
+let wolfBottomRight = {x: 155, y:50, w: 50, h: 60};
+let wolfTopLeft = {x: 95, y: 50, w: 50, h: 60};
+let wolfBottomLeft = {x: 95, y: 50, w: 50, h: 60};
+let eggTopLeft = {x: 73, y: 36, w: 5, h:7};
+let eggTopRight = {x: 225, y: 36, w: 5, h:7};
+let eggBottomRight = {x: 225, y: 65, w: 5, h:7};
+let eggBottomLeft = {x: 73, y: 65, w: 5, h:7};
+
 function init() {
     let ctx = document.getElementById("canvas").getContext("2d");
     let wolfImgTopRight = document.getElementById('wolfTopRight');
     let wolfImgBottomRight = document.getElementById('wolfBottomRight');
     let wolfImgTopLeft = document.getElementById('wolfTopLeft');
     let wolfImgBottomLeft = document.getElementById('wolfBottomLeft');
+    let egg = document.getElementById('theEgg');
     //let chickenImgLeft = document.getElementById('chickenLeft');
-    let wolfTopRight = {x: 155, y:50, w: 50, h: 60};
-    let wolfBottomRight = {x: 155, y:50, w: 50, h: 60};
-    let wolfTopLeft = {x: 95, y: 50, w: 50, h: 60};
-    let wolfBottomLeft = {x: 95, y: 50, w: 50, h: 60};
-   // debugger;
 
-    window.addEventListener('keydown', kbdHangler);
+    let caught = false;
+    let score = 0;
+    let positionToFallFrom = 1;
+    // debugger;
 
-    function kbdHangler(event){
+    window.addEventListener('keydown', kbdHandler);
+
+    setInterval(fallingEggs, 400);
+    function kbdHandler(event){
         //console.log(event.code);
         switch(event.code){
             case 'Numpad7': drawTopLeft();
@@ -34,54 +45,66 @@ function init() {
 
 
     function drawTopRight() {
-        ctx.clearRect(0, 0, 745, 470)
+        ctx.clearRect(0, 0, 745, 470);
         ctx.drawImage(wolfImgTopRight, wolfTopRight.x, wolfTopRight.y, wolfTopRight.w, wolfTopRight.h);
 
     }
 
     function drawBottomRight() {
-        ctx.clearRect(0, 0, 745, 470)
+        ctx.clearRect(0, 0, 745, 470);
         ctx.drawImage(wolfImgBottomRight, wolfBottomRight.x, wolfBottomRight.y, wolfBottomRight.w, wolfBottomRight.h);
     }
 
     function drawTopLeft(){
-        ctx.clearRect(0, 0, 745, 470)
+        ctx.clearRect(0, 0, 745, 470);
 
         ctx.drawImage(wolfImgTopLeft, wolfTopLeft.x, wolfTopLeft.y, wolfTopLeft.w, wolfTopLeft.h);
         //c.fillRect(x, y, width, height);
     }
 
     function drawBottomLeft(){
-        ctx.clearRect(0, 0, 745, 470)
+        ctx.clearRect(0, 0, 745, 470);
         ctx.drawImage(wolfImgBottomLeft, wolfBottomLeft.x, wolfBottomLeft.y, wolfBottomLeft.w, wolfBottomLeft.h);
         //c.fillRect(x, y, width, height);
     }
-    
-    function fallingEggs() {
-        let positionToFallFrom = parseInt(Math.random() * (4)) + 1;
-        if (positionToFallFrom === 1) {
-            generatingPosition(positionToFallFrom)
-        } else if (positionToFallFrom === 2) {
-            generatingPosition(positionToFallFrom)
-        } else if (positionToFallFrom === 3) {
-            generatingPosition(positionToFallFrom)
+
+    function fallingEggs(position) {
+        if (position === 1) {
+            animate(eggTopLeft, 5, 3, wolfTopLeft.x);
+        } else if (position === 2) {
+            animate(eggTopRight, -5, 3, wolfTopRight.x);
+        } else if (position === 3) {
+            animate(eggBottomRight, -5, 3, wolfBottomRight.x);
         } else {
-            generatingPosition(positionToFallFrom)
+            animate(eggBottomLeft, 5, 3, wolfBottomLeft.x);
         }
 
-        function generatingPosition(position) {
-            switch (position) {
-                case 1:
-
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
+    }
+    function animate(eggToFall, moveRateX, moveRateY, wolfCoordinateX) {
+        eggToFall.x += moveRateX;
+        eggToFall.y += moveRateY;
+        if(eggToFall === eggBottomLeft || eggToFall === eggTopLeft) {
+            if(eggToFall.x >= wolfCoordinateX ){
+                fallingEggs(generateRandomPosition());
+            } else {
+                ctx.drawImage(egg, eggToFall.x, eggToFall.y, eggToFall.w, eggToFall.h);
             }
+            clear(eggToFall, -moveRateX, -moveRateY);
+        } else if (eggToFall === eggTopRight || eggToFall === eggBottomRight) {
+            if(eggToFall.x < wolfCoordinateX + 50 ){
+                fallingEggs(generateRandomPosition());
+            } else {
+                ctx.drawImage(egg, eggToFall.x, eggToFall.y, eggToFall.w, eggToFall.h);
+            }
+            clear(eggToFall, -moveRateX, -moveRateY);
         }
+
+    }
+    function clear(eggToFall, offsetX, offsetY) {
+        ctx.clearRect(eggToFall.x + offsetX, eggToFall.y + offsetY, eggToFall.w, eggToFall.h+1);
+    }
+    function generateRandomPosition() {
+        return parseInt(Math.random() * (4)) + 1;
     }
 }
 
